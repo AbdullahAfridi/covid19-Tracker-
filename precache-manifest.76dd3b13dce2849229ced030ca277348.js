59c36cb4402c22f01b37b44eba650d3d67caffee
abdullahdaulatzia@gmail.com <abdullahdaulatzai@gmail.com>
2020-09-01 23:27:11 +0530

    Updates

---
+++ precache-manifest.76dd3b13dce2849229ced030ca277348.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec4d27de77b66a5ead06cc524fc0510e",
+    "url": "/http//AbdullahAfridi.github.io/covid19update/index.html"
+  },
+  {
+    "revision": "b2438e67e532e7896fc0",
+    "url": "/http//AbdullahAfridi.github.io/covid19update/static/css/main.55daf951.chunk.css"
+  },
+  {
+    "revision": "400cd8caba5c871af9f0",
+    "url": "/http//AbdullahAfridi.github.io/covid19update/static/js/2.4f71ffa1.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/http//AbdullahAfridi.github.io/covid19update/static/js/2.4f71ffa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2438e67e532e7896fc0",
+    "url": "/http//AbdullahAfridi.github.io/covid19update/static/js/main.f4ee1a31.chunk.js"
+  },
+  {
+    "revision": "ee214f931f5cbaa7c002",
+    "url": "/http//AbdullahAfridi.github.io/covid19update/static/js/runtime-main.e735b035.js"
+  }
+]);
